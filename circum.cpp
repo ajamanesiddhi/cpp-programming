773e3652f8a315c5f1a06f28732cc33061f99c02
ajamanesiddhi <ajamanesiddhi@gmail.com>
2026-07-24 13:08:44 +0530

    Add files via upload

---
+++ circum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float radius,area,circumference;
+cout<<"enter radius:";
+cin>>radius;
+area=3.14*radius*radius;
+circumference=2*3.14*radius;
+cout<<"Area="<<area<<endl;
+cout<<"Circumference="<<circumference;
+return 0;
+}
